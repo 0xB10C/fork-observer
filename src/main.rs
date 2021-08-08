a4797f9738cf1debaefc8bc63735af30c8869ef1
0xb10c <0xb10c@gmail.com>
2021-08-08 18:33:23 +0200

    quick and dirty zwischenstand

---
+++ src/main.rs
@@ -0,0 +1,228 @@
+use std::collections::HashSet;
+use std::convert::{TryInto};
+use std::sync::Arc;
+
+// TODO: remove?
+use std::convert::Infallible;
+
+use tokio::sync::Mutex;
+use tokio::task;
+use tokio::time;
+
+use warp::Filter;
+
+use zerocopy::{byteorder::U64, AsBytes, LayoutVerified};
+
+use bitcoincore_rpc::bitcoin::hash_types::BlockHash;
+use bitcoincore_rpc::json::{GetChainTipsResult, GetChainTipsResultStatus};
+use bitcoincore_rpc::{Auth, Client, RpcApi};
+
+mod types;
+
+use types::{TipInfo, TipInfoJson, JsonResponse, TipInfoKey, BlockInfo, BlockInfoJson, BlockInfoKey};
+
+
+async fn get_tips(rpc: Rpc) -> GetChainTipsResult {
+    let res = task::spawn_blocking(move || {
+        return rpc.get_chain_tips().unwrap();
+    })
+    .await;
+    res.unwrap()
+}
+
+async fn write_to_db(db_adds: &HashSet<(BlockInfoKey, BlockInfo)>, tips: &GetChainTipsResult, db: Db) {
+    let mut batch = sled::Batch::default();
+    for (k, v) in db_adds.iter() {
+        batch.insert(k.as_bytes(), v.as_bytes());
+    }
+
+    // remove data about all tips from the db
+    for kv_option in db
+        .lock()
+        .await
+        .range(TipInfoKey::new(&BlockHash::default()).as_bytes()..)
+    {
+        let (k,_) = kv_option.unwrap();
+        batch.remove(k);
+    }
+
+    for tip in tips.iter() {
+        batch.insert(TipInfoKey::new(&tip.hash).as_bytes(), TipInfo::new(&tip).as_bytes());
+    }
+
+    db.lock().await.apply_batch(batch).unwrap();
+
+}
+
+async fn process_tips(
+    tips: &GetChainTipsResult,
+    known_tips: &HashSet<BlockHash>,
+    rpc: Arc<Client>,
+) -> HashSet<(BlockInfoKey, BlockInfo)> {
+    let mut db_adds: HashSet<(BlockInfoKey, BlockInfo)> = HashSet::new();
+
+    let min_height = tips.iter().min_by_key(|tip| tip.height).unwrap().height;
+    let scan_start_height = std::cmp::max(min_height as i64 - 3, 0);
+
+
+    let active_tip = tips
+        .iter()
+        .filter(|tip| tip.status == GetChainTipsResultStatus::Active)
+        .last()
+        .unwrap();
+    if !known_tips.contains(&active_tip.hash) {
+        let mut next_header = active_tip.hash;
+        for i in 0..=(active_tip.height as i64 - scan_start_height) {
+
+            if known_tips.contains(&next_header) {
+                break;
+            }
+
+            let header = rpc
+                .get_block_header(&next_header)
+                .unwrap();
+
+            let key = BlockInfoKey::new(active_tip.height - i as u64, &header.block_hash());
+
+            let header_bytes = bitcoincore_rpc::bitcoin::consensus::serialize(&header);
+            let value = BlockInfo {
+                height: U64::new(active_tip.height - i as u64),
+                header: header_bytes[0..80].try_into().unwrap(),
+            };
+
+            db_adds.insert((key, value));
+            next_header = header.prev_blockhash;
+        }
+    }
+
+    for inactiv_tip in tips
+        .iter()
+        .filter(|tip| tip.status != GetChainTipsResultStatus::Active)
+    {
+        if !known_tips.contains(&inactiv_tip.hash) {
+            let mut next_header = inactiv_tip.hash;
+            for i in 0..=inactiv_tip.branch_length {
+                if known_tips.contains(&next_header) {
+                    break;
+                }
+
+                let header = rpc
+                    .get_block_header(&next_header)
+                    .unwrap();
+
+
+                let key = BlockInfoKey::new(inactiv_tip.height - i as u64, &header.block_hash());
+
+                let header_bytes = bitcoincore_rpc::bitcoin::consensus::serialize(&header);
+                let value = BlockInfo {
+                    height: U64::new(inactiv_tip.height - i as u64),
+                    header: header_bytes[0..80].try_into().unwrap(),
+                };
+
+                db_adds.insert((key, value));
+                next_header = header.prev_blockhash;
+            }
+        }
+    }
+
+    return db_adds;
+}
+
+type Db = Arc<Mutex<sled::Db>>;
+type Rpc = Arc<Client>;
+
+#[tokio::main]
+async fn main() {
+    let db: Db = Arc::new(Mutex::new(sled::open("my_db").unwrap()));
+    let rpc: Rpc = Arc::new(
+        Client::new(
+            "http://127.0.0.1:38332",
+            Auth::UserPass(
+                "__cookie__".to_string(),
+                "".to_string(),
+            ),
+        )
+        .unwrap(),
+    );
+
+    let mut interval = time::interval(time::Duration::from_secs(10));
+
+    let db_write = db.clone();
+    task::spawn(async move {
+        let mut known_tips: HashSet<BlockHash> = HashSet::new();
+        loop {
+            let db_write = db_write.clone();
+            let tips = get_tips(rpc.clone()).await;
+            let db_adds = process_tips(&tips, &known_tips, rpc.clone()).await;
+            write_to_db(&db_adds, &tips, db_write).await;
+            known_tips = tips.iter().map(|tip| tip.hash).collect();
+            interval.tick().await;
+        }
+    });
+
+    let index_html = warp::get()
+        .and(warp::path::end())
+        .and(warp::fs::file("./d3-blocktree/index.html"));
+
+    let style_css = warp::get()
+        .and(warp::path!("css" / "style.css"))
+        .and(warp::fs::file("./d3-blocktree/css/style.css"));
+
+    let blocktree_js = warp::get()
+        .and(warp::path!("js" / "blocktree.js"))
+        .and(warp::fs::file("./d3-blocktree/js/blocktree.js"));
+
+    let data_json = warp::get()
+        .and(warp::path("data.json"))
+        .and(with_db(db))
+        .and_then(sleepy);
+
+    let routes = index_html.or(blocktree_js).or(style_css).or(data_json);
+
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
+
+fn with_db(db: Db) -> impl Filter<Extract = (Db,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || db.clone())
+}
+
+async fn sleepy(db: Db) -> Result<impl warp::Reply, Infallible> {
+    let r = response(db).await;
+    Ok(warp::reply::json(&r))
+}
+
+async fn response(db: Db) -> JsonResponse {
+
+    let start_key = BlockInfoKey::new(0, &BlockHash::default());
+    let end_key = BlockInfoKey::new(100, &BlockHash::default());
+
+    let mut block_infos = vec![];
+
+    for kv_option in db
+        .lock()
+        .await
+        .range(start_key.as_bytes()..end_key.as_bytes())
+    {
+        let (_, v_bytes) = kv_option.unwrap();
+        let layout: LayoutVerified<&[u8], BlockInfo> =
+            LayoutVerified::new_unaligned(&*v_bytes).expect("bytes do not fit schema");
+        let block_info: &BlockInfo = layout.into_ref();
+        block_infos.push(BlockInfoJson::new(block_info));
+    }
+
+    let mut tip_infos = vec![];
+
+    for kv_option in db
+        .lock()
+        .await
+        .range(TipInfoKey::new(&BlockHash::default()).as_bytes()..)
+    {
+        let (_, v_bytes) = kv_option.unwrap();
+        let layout: LayoutVerified<&[u8], TipInfo> =
+            LayoutVerified::new_unaligned(&*v_bytes).expect("bytes do not fit schema");
+        let tip_info: &TipInfo = layout.into_ref();
+        tip_infos.push(TipInfoJson::new(tip_info));
+    }
+
+    JsonResponse{tip_infos: tip_infos, block_infos: block_infos}
+}
---
+++ src/types.rs
@@ -0,0 +1,185 @@
+
+use std::convert::{TryInto, TryFrom};
+use byteorder::{BigEndian, LittleEndian};
+use zerocopy::{byteorder::U128, byteorder::U64, AsBytes, FromBytes, Unaligned};
+use serde::Serialize;
+use bitcoincore_rpc::json::{ GetChainTipsResultTip, GetChainTipsResultStatus};
+use bitcoincore_rpc::bitcoin::hash_types::BlockHash;
+use bitcoincore_rpc::bitcoin::BlockHeader;
+use bitcoincore_rpc::bitcoin::hashes::Hash;
+
+
+pub const PREFIX_BLOCKINFO: u8 = b'b';
+pub const PREFIX_TIPINFO: u8 = b't';
+
+#[repr(u8)]
+pub enum TipStatus {
+    Active = b'a',
+    HeadersOnly = b'h',
+    Invalid = b'i',
+    ValidFork = b'f',
+    ValidHeaders = b'p',
+}
+
+impl From<GetChainTipsResultStatus> for TipStatus {
+    fn from(status: GetChainTipsResultStatus) -> Self {
+        match status {
+            GetChainTipsResultStatus::Active => TipStatus::Active,
+            GetChainTipsResultStatus::HeadersOnly => TipStatus::HeadersOnly ,
+            GetChainTipsResultStatus::Invalid => TipStatus::Invalid,
+            GetChainTipsResultStatus::ValidFork => TipStatus::ValidFork,
+            GetChainTipsResultStatus::ValidHeaders => TipStatus::ValidHeaders,
+        }
+    }
+}
+
+impl TryFrom<u8> for TipStatus {
+    type Error = &'static str;
+
+    fn try_from(status: u8) -> Result<Self, Self::Error> {
+        match status {
+            b'a' => Ok(TipStatus::Active),
+            b'h' => Ok(TipStatus::HeadersOnly) ,
+            b'i' => Ok(TipStatus::Invalid),
+            b'f' => Ok(TipStatus::ValidFork),
+            b'p' => Ok(TipStatus::ValidHeaders),
+            _ => Err("Invalid TipStatus")
+        }
+    }
+}
+
+impl Into<u8> for TipStatus {
+    fn into(self) -> u8 {
+        match self {
+            TipStatus::Active => TipStatus::Active as u8,
+            TipStatus::HeadersOnly => TipStatus::HeadersOnly as u8,
+            TipStatus::Invalid => TipStatus::Invalid as u8,
+            TipStatus::ValidFork => TipStatus::ValidFork as u8,
+            TipStatus::ValidHeaders => TipStatus::ValidHeaders as u8,
+        }
+    }
+}
+
+
+impl ToString for TipStatus {
+    fn to_string(&self) -> String {
+        match self {
+            TipStatus::Active => String::from("active"),
+            TipStatus::HeadersOnly => String::from("headers-only"),
+            TipStatus::Invalid => String::from("invalid"),
+            TipStatus::ValidFork => String::from("valid-fork"),
+            TipStatus::ValidHeaders => String::from("valid-headers"),
+        }
+    }
+}
+
+
+
+#[derive(FromBytes, AsBytes, Unaligned, Eq, Hash, PartialEq)]
+#[repr(C)]
+pub struct BlockInfoKey {
+    prefix: u8,
+    height: U64<BigEndian>,
+    hash_part: U128<BigEndian>,
+}
+
+impl BlockInfoKey {
+    pub fn new(height: u64, block_hash: &BlockHash) -> BlockInfoKey {
+        BlockInfoKey{prefix: PREFIX_BLOCKINFO, height: U64::new(height), hash_part: U128::new(short_hash(&block_hash))}
+    }
+}
+
+
+#[derive(FromBytes, AsBytes, Unaligned, Debug, Eq, Hash, PartialEq)]
+#[repr(C)]
+pub struct BlockInfo {
+    pub height: U64<LittleEndian>,
+    pub header: [u8; 80],
+}
+
+#[derive(FromBytes, AsBytes, Unaligned, Eq, Hash, PartialEq)]
+#[repr(C)]
+pub struct TipInfoKey {
+    prefix: u8,
+    hash_part: U128<BigEndian>,
+}
+
+impl TipInfoKey {
+    pub fn new(block_hash: &BlockHash) -> TipInfoKey {
+        TipInfoKey{prefix: PREFIX_TIPINFO, hash_part: U128::new(short_hash(&block_hash))}
+    }
+}
+
+#[derive(FromBytes, AsBytes, Unaligned, Debug, Eq, Hash, PartialEq)]
+#[repr(C)]
+pub struct TipInfo {
+    height: U64<LittleEndian>,
+    hash: [u8; 32],
+    status: u8,
+}
+
+impl TipInfo {
+    pub fn new(tip: &GetChainTipsResultTip) -> TipInfo {
+        TipInfo{height: U64::new(tip.height), hash: tip.hash[0..32].try_into().unwrap(), status: TipStatus::from(tip.status).into()}
+    }
+}
+
+
+#[derive(Serialize)]
+pub struct BlockInfoJson {
+    pub block_height: u64,
+    pub hash: String,
+    pub prev: String,
+}
+
+impl BlockInfoJson {
+    pub fn new(block_info: &BlockInfo) -> BlockInfoJson {
+        let header: BlockHeader =
+            bitcoincore_rpc::bitcoin::consensus::deserialize(&block_info.header).unwrap();
+        BlockInfoJson {
+            block_height: u64::from(block_info.height),
+            hash: header.block_hash().to_string(),
+            prev: if header.prev_blockhash == BlockHash::default() {
+                "".to_string()
+            } else {
+                header.prev_blockhash.to_string()
+            },
+        }
+    }
+}
+
+#[derive(Serialize)]
+pub struct TipInfoJson {
+    pub block_height: u64,
+    pub hash: String,
+    pub status: String,
+}
+
+impl TipInfoJson {
+    pub fn new(tip_info: &TipInfo) -> TipInfoJson {
+        let block_hash = BlockHash::from_inner(tip_info.hash);
+
+        TipInfoJson {
+            block_height: u64::from(tip_info.height),
+            hash: block_hash.to_string(),
+            status: TipStatus::try_from(tip_info.status).unwrap().to_string()
+        }
+    }
+}
+
+
+#[derive(Serialize)]
+pub struct JsonResponse {
+    pub block_infos: Vec<BlockInfoJson>,
+    pub tip_infos: Vec<TipInfoJson>,
+}
+
+
+fn short_hash(hash: &BlockHash) -> u128 {
+    assert_eq!(hash.len(), 32);
+    let mut short_hash = 0u128;
+    for i in 15..=31 {
+        short_hash = short_hash + ((hash[i] as u128) << i) as u128;
+    }
+    short_hash
+}
